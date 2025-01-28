75c7cc2ecfb935e7d487a68f23815cadbe1d59ad
Vinicius CR <eu@viniciuscr.com.br>
2025-01-28 15:27:00 +0100

    chore: add @vercel/speed-insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { ColorSchemeScript } from "@mantine/core";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Document() {
   return (
     <Html lang="en">
@@ -10,6 +10,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   );
